99a098988acfd2776a2b22614867f043e48ff453
laprasdrum <laprasdrum@yuyacat.com>
2021-05-21 10:38:46 +0900

    define component

--- chap5/app.js
+++ chap5/app.js
@@ -1,22 +1,24 @@
-Vue.createApp({
-    // local component (.component は global component)
-    // 同じ定義が global にもある場合、local が優先される
-    components: {
-        'my-title': {
-            data() {
-                return {
-                    title: '2',
-                }
-            },
-            template: '#title-template',
-        }
+const myTitle = {
+  data() {
+    return {
+      title: '2',
     }
+  },
+  template: '#title-template',
+}
+
+Vue.createApp({
+  // local component (.component は global component)
+  // 同じ定義が global にもある場合、local が優先される
+  components: {
+    'my-title': myTitle,
+  },
 })
 .component('my-title', {
-    data() {
-        return {
-            title: 'text here',
-        }
-    },
-    template: `<input type="date" v-model="title" />`,
+  data() {
+    return {
+      title: 'text here',
+    }
+  },
+  template: `<input type="date" v-model="title" />`,
 }).mount('#app')
